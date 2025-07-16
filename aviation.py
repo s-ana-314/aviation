2b5d12956b41b554eb43e3b55daa0db04f388ecc
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2025-07-16 16:20:53 +0100

    minor comments and spelling edits

--- aviation.py
+++ aviation.py
@@ -1,7 +1,7 @@
-days_per_year = 365.0
+days_per_year = 366.0  # leap year 
 
-passengers_per_year= 6_000_000_000.0 #note use float 
-seats_per_aircraft= 180.0
+passengers_per_year= 5_000_000_000.0 # note use float 
+seats_per_aircraft= 180.0 # estimated from observation as 30 rows x 6 seats per row
 flight_per_aircraft_per_day= 2.0
 
 passengers_per_day= passengers_per_year/days_per_year

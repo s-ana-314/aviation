c30cf8dff0eedb80a16b5f311b0c5e971163744f
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2025-07-17 12:22:57 +0100

    reformated using ruff

--- src/aviation/aviation.py
+++ src/aviation/aviation.py
@@ -2,7 +2,5 @@ def passengers_per_day(passengers_per_year, days_per_year):
     return passengers_per_year / days_per_year
 
 
-def required_global_fleet(
-    passengers_per_day, seats_per_aircraft, flight_per_aircraft_per_day
-):
+def required_global_fleet(passengers_per_day, seats_per_aircraft, flight_per_aircraft_per_day):
     return passengers_per_day / (seats_per_aircraft * flight_per_aircraft_per_day)

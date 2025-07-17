d9b542b17311aa9577da45cd49744b95c05e33ca
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2025-07-17 09:51:54 +0100

    refarcotring python model into functions

--- aviation.py
+++ aviation.py
@@ -1,10 +1,6 @@
-days_per_year = 366.0  # leap year 
 
-passengers_per_year= 5_000_000_000.0 # note use float 
-seats_per_aircraft= 180.0 # estimated from observation as 30 rows x 6 seats per row
-flight_per_aircraft_per_day= 2.0
+def passengers_per_day(passengers_per_year, days_per_year):
+    return passengers_per_year/days_per_year
 
-passengers_per_day= passengers_per_year/days_per_year
-required_global_fleet= passengers_per_day/ (seats_per_aircraft*flight_per_aircraft_per_day)
-
-print(f"{required_global_fleet=:.2f}") #f string good for formating
+def required_global_fleet(passengers_per_day, seats_per_aircraft, flight_per_aircraft_per_day):
+    return passengers_per_day/ (seats_per_aircraft*flight_per_aircraft_per_day)
---
+++ required_global_fleet.py
@@ -0,0 +1,12 @@
+import aviation
+
+days_per_year = 366.0  # leap year 
+
+passengers_per_year= 5_000_000_000.0 # note use float 
+seats_per_aircraft= 180.0 # estimated from observation as 30 rows x 6 seats per row
+flight_per_aircraft_per_day= 2.0
+
+passengers_per_day= aviation.passengers_per_day(passengers_per_year, days_per_year)
+required_global_fleet= aviation.required_global_fleet(passengers_per_day,seats_per_aircraft,flight_per_aircraft_per_day)
+
+print(f"{required_global_fleet=:.2f}") #f string good for formating

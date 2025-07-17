f6ed3058db8cdf9d62405642f54ee66ac8683546
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2025-07-17 10:55:06 +0100

    made analysis directory for python project

--- aviation.py
+++
@@ -1,6 +0,0 @@
-
-def passengers_per_day(passengers_per_year, days_per_year):
-    return passengers_per_year/days_per_year
-
-def required_global_fleet(passengers_per_day, seats_per_aircraft, flight_per_aircraft_per_day):
-    return passengers_per_day/ (seats_per_aircraft*flight_per_aircraft_per_day)

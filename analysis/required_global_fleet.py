484bc21999f0381b51149f1ab06507d513766bee
Ana Stojanovic <stojanovic.ana.314@gmail.com>
2025-07-23 11:15:17 +0100

    implemented systems model and testing

--- analysis/required_global_fleet.py
+++ analysis/required_global_fleet.py
@@ -3,10 +3,20 @@
 import aviation
 
 days_per_year = 366.0  # leap year
-
 passengers_per_year = 5_000_000_000.0  # note use float
 seats_per_aircraft = 180.0  # estimated from observation as 30 rows x 6 seats per row
 flight_per_aircraft_per_day = 2.0
+
+inputs = {
+    "passengers_per_year": passengers_per_year,
+    "days_per_year": days_per_year,
+    "seats_per_aircraft": seats_per_aircraft,
+    "flight_per_aircraft_per_day": flight_per_aircraft_per_day,
+}
+output = "required_global_fleet"
+
+# systems_model = engine.SystemsModel(aviation.tranforms)
+# required_global_fleet = systems_model.evaluate(inputs, output)
 
 passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
 required_global_fleet = aviation.required_global_fleet(
